bd21f5e97b180e2002883c666c30be2fa533ba72
Bikram Sahoo <bikramsahoo@live.in>
2024-02-26 05:09:11 +0530

    Add favicon ico and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mo Chat App.
+## version 0.0.1
+### Public Chat Application.
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'view')));
+app.use(express.static(path.join(__dirname,'public')));
 let PORT = process.env.PORT||3000;
 
 
